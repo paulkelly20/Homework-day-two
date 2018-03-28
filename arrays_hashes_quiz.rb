4f8bfb6f3eadaf9c5689ed627cbfcf87aa8cca96
Paul Kelly <paul.kelly20@gmail.com>
2018-03-28 16:31:12 +0100

    questions 1_2_3 of exercise b '

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -1,53 +1,53 @@
-# Homework
-
-## Exercise A
-
-### Given the following data structure:
-
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-
-### Complete these tasks:
-
-# 1. Add `"Edinburgh Waverley"` to the end of the array
-stops.push("Edinburgh Waverley")
-
-# 2. Add `"Glasgow Queen St"` to the start of the array
-stops.insert(0,"Glasgow Queen St")
-p stops
-# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-stops.insert(4,"Polmont")
-p stops
-# 4. Work out the index position of `"Linlithgow"`
-linlithgow = stops.index("Linlithgow")
-p linlithgow
-
-# 5. Remove `"Livingston"` from the array using its name
-stops.delete("Livingston")
-p stops
-# 6. Delete `"Cumbernauld"` from the array by index
-stops.delete_at(2)
-p stops
-# 7. How many stops there are in the array?
-stops_in_array = stops.count
-p stops_in_array
-# 8. How many ways can we return `"Falkirk High"` from the array?
-p stops[2]
-p stops[-5]
-falkirk_stop = stops.include? "Falkirk High"
-p falkirk_stop
-# 9. Reverse the positions of the stops in the array
-p stops.reverse!
-# 10. Print out all the stops using a for loop
-def train_stops(stop)
-
-  for station in stop
-
-    end
-end
-p train_stops(stops)
-## Exercise B
-
-### Given the following data structure:
+# # Homework
+#
+# ## Exercise A
+#
+# ### Given the following data structure:
+#
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+#
+# ### Complete these tasks:
+#
+# # 1. Add `"Edinburgh Waverley"` to the end of the array
+# stops.push("Edinburgh Waverley")
+#
+# # 2. Add `"Glasgow Queen St"` to the start of the array
+# stops.insert(0,"Glasgow Queen St")
+# p stops
+# # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# stops.insert(4,"Polmont")
+# p stops
+# # 4. Work out the index position of `"Linlithgow"`
+# linlithgow = stops.index("Linlithgow")
+# p linlithgow
+#
+# # 5. Remove `"Livingston"` from the array using its name
+# stops.delete("Livingston")
+# p stops
+# # 6. Delete `"Cumbernauld"` from the array by index
+# stops.delete_at(2)
+# p stops
+# # 7. How many stops there are in the array?
+# stops_in_array = stops.count
+# p stops_in_array
+# # 8. How many ways can we return `"Falkirk High"` from the array?
+# p stops[2]
+# p stops[-5]
+# falkirk_stop = stops.include? "Falkirk High"
+# p falkirk_stop
+# # 9. Reverse the positions of the stops in the array
+# p stops.reverse!
+# # 10. Print out all the stops using a for loop
+# def train_stops(stop)
+#
+#   for station in stop
+#
+#     end
+# end
+# p train_stops(stops)
+# ## Exercise B
+#
+# ### Given the following data structure:
 
 users = {
   "Jonathan" => {
@@ -108,8 +108,11 @@ users = {
 ### Complete these tasks:
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
 # 4. Get the type of Avril's pet Monty
 # 5. Get the smallest of Erik's lottery numbers
 # 6. Return an array of Avril's lottery numbers that are even
